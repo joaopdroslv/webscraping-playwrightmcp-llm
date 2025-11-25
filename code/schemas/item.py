4fffb27ac3d0ba0586fd3196c11650fae6ee01cf
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-11-25 15:42:23 -0300

    item detailed and item detailed output schemas created

--- code/schemas/item.py
+++ code/schemas/item.py
@@ -1,11 +1,35 @@
 from typing import List
-from pydantic import BaseModel
+
+from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
-    ref: str
+    url: str
     name: str
 
 
 class ItemsOutput(BaseModel):
     items: List[Item]
+
+
+class ItemDetailed(BaseModel):
+    ref: str
+    city: str
+    neighborhood: str
+    category: str = Field(examples=["Casa", "Terreno"])
+    application: str = Field(examples=["Residencial", "Comercial"])
+    value: float
+
+    bedroom_count: int
+    bathroom_count: int
+    commom_room_count: int
+    kitchen_count: int
+    has_service_area: bool
+
+    total_area: float
+    built_area: float
+    material: str = Field(examples=["Alvenaria", "Madeira"])
+
+
+class ItemsDetailedOutput(BaseModel):
+    items_detailed: List[Item]
