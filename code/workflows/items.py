629a111d7ee769ef56ca1a184a4ae00d5fd22654
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-11-26 00:01:28 -0300

    code splitted and organized

---
+++ code/workflows/items.py
@@ -0,0 +1,65 @@
+from code.gpt_model import gpt_model
+from code.logs import logger
+from code.mcp import BrowserMCP
+from code.prompts.items import get_item_details_prompt, found_items_prompt
+from code.schemas.items import ItemDetailed, ItemsOutput
+from code.utils import make_dir, write_into_xlsx
+from datetime import datetime
+
+
+async def items_workflow() -> None:
+
+    browser_mcp = BrowserMCP()
+
+    PAGE_URL = "https://www.samburaimoveis.com.br/busca_avancada?finalidade=Venda"
+    run_id = "runId_" + str(int(datetime.now().timestamp()))
+    run_dir_path = make_dir(dir_name=run_id)
+
+    logger.info(run_id)
+    logger.info(run_dir_path)
+
+    snapshot = await browser_mcp.extract_snapshot(url=PAGE_URL)
+
+    items_model = gpt_model.with_structured_output(schema=ItemsOutput)
+
+    full_prompt = (
+        found_items_prompt
+        + f"""
+    \nHere is the snapshot:
+    {snapshot}
+    """
+    )
+    response = items_model.invoke(full_prompt)
+
+    items_ouput: ItemsOutput = response
+    items_list = items_ouput.items
+
+    item_details_model = gpt_model.with_structured_output(ItemDetailed)
+
+    items_detailed_list = []
+    for item in items_list:
+        logger.info(f"Processing URL: \n{item.url}")
+
+        if not "/imovel" in item.url:
+            logger.info("[ERROR] Item has no /imovel in its URL")
+            continue
+
+        snapshot = await browser_mcp.extract_snapshot(url=item.url)
+
+        full_prompt = (
+            get_item_details_prompt
+            + f"""
+        \nHere is the snapshot:
+        {snapshot}
+        """
+        )
+        response: ItemDetailed = item_details_model.invoke(full_prompt)
+
+        item_ref = response.ref
+        scheenshot_filename = run_dir_path + f"/{run_id}_{item_ref}.png"
+        await browser_mcp.take_screenshot(url=item.url, filename=scheenshot_filename)
+
+        items_detailed_list.append(response.model_dump())
+
+    output_xlsx_filename = run_dir_path + f"/{run_id}_output.xlsx"
+    write_into_xlsx(output=items_detailed_list, filename=output_xlsx_filename)
---
+++ code/workflows/products.py
@@ -0,0 +1,63 @@
+from code.gpt_model import gpt_model
+from code.logs import logger
+from code.mcp import BrowserMCP
+from code.schemas.products import ProductsOutput
+
+
+async def products_workflow() -> None:
+
+    browser_mcp = BrowserMCP()
+
+    PAGE_URL = "https://www.kabum.com.br/"
+
+    snapshot = await browser_mcp.extract_snapshot(url=PAGE_URL)
+
+    response = gpt_model.invoke(
+        f"""
+    Here is a page snapshot. Identify the search input bar
+    (where the user types the product name) and return ONLY the "ref"
+    value, nothing else.
+
+    Respond only the ref ID (ex: e315). No explanation.
+
+    Snapshot:
+    ```yaml
+        {snapshot}
+    ```
+    """
+    )
+
+    ref = response.content
+
+    snapshot = await browser_mcp.query_product(
+        url=PAGE_URL,
+        input_ref=ref,
+        query="AMD Ryzen 7 5800X3D",
+    )
+
+    products_model = gpt_model.with_structured_output(schema=ProductsOutput)
+
+    response = products_model.invoke(
+        f"""
+    You will receive a page snapshot from an ecommerce website.
+    Extract every product listed on the page.
+
+    Return a JSON array where each item has:
+    - name (string)
+    - price (number)
+
+    If the price is not present, omit the item.
+
+    Respond ONLY with valid JSON, nothing else.
+
+    Here's the snapshot:
+    {snapshot}
+    """
+    )
+
+    products_output: ProductsOutput = response
+    product_list = products_output.products
+
+    logger.info(f"Found a total of [ {len(product_list)} ] products")
+    for i, p in enumerate(product_list):
+        logger.info(f"[ {i + 1} ] {p.name.split(",")[0]} | {p.price}")
