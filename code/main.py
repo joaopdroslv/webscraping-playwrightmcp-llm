e33e64147f8fcd10bf4c7b31c9eb091a994944af
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-12-03 12:45:58 -0300

    test connection function created

--- code/main.py
+++ code/main.py
@@ -1,5 +1,20 @@
 import asyncio
+from code.mcp import BrowserMCP
 from code.workflows.items import items_workflow
 
+
+async def test_connection():
+    """Util function to test the connection to a web page using the Playwright MCP."""
+
+    PAGE_URL = "https://www.zapimoveis.com.br/venda/imoveis/sp+presidente-prudente/?transacao=venda"
+    SCREENSHOT_FILENAME = "zapimoveis.png"
+
+    browser_mcp = BrowserMCP(base_url="http://localhost:8931")
+    await browser_mcp.take_screenshot(
+        page_url=PAGE_URL,
+        filename=SCREENSHOT_FILENAME,
+    )
+
+
 if __name__ == "__main__":
-    asyncio.run(items_workflow())
+    asyncio.run(test_connection())
